fe68b18e1a384689040e1ab43218a3d7765ed564
8ad40n <joy184110@gmail.com>
2024-08-24 18:26:52 +0600

    Post api correct

--- src/app/(auth)/signup/api/route.js
+++ src/app/(auth)/signup/api/route.js
@@ -2,29 +2,32 @@ import { connectDB } from "@/lib/connectDB";
 import { NextResponse } from "next/server";
 
 export const POST = async (request) => {
-    const newUser = await request.json();
-    try {
-        const db = connectDB();
-        const userCollection = db.collection("users");
+  const newUser = await request.json();
+  try {
+    const db = await connectDB();
+    const userCollection = db.collection("users");
 
-        const exists = await userCollection.findOne({ email: newUser.email });
-        if (exists) {
-            return NextResponse.json(
-                { message: "User exists!" },
-                { status: 409 } 
-            );
-        }
-
-        const res = await userCollection.insertOne(newUser);
-        return NextResponse.json(
-            { message: "User created!", data: res },
-            { status: 201 }
-        );
-    } catch (error) {
-        return NextResponse.json(
-            { message: "Something went wrong!" },
-            { status: 500 } 
-        );
-        
+    const exists = await userCollection.findOne({ email: newUser.email });
+    if (exists) {
+      return NextResponse.json(
+        { message: "User exists!" },
+        { status: 409 }
+      );
     }
-}
+
+    const res = await userCollection.insertOne(newUser);
+    return NextResponse.json(
+      { message: "User created!", data: res },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Something went wrong!", error: error.message },
+      { status: 500 }
+    );
+  }
+};
+
+export const GET = async () => {
+  return new Response('Hello from the server!');
+};
--- src/lib/connectDB.js
+++ src/lib/connectDB.js
@@ -1,28 +1,23 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
 let db;
-export const connectDB = async()=>{
-    if(db) return db;
-    
-    try{
-        const uri = process.env.NEXT_PUBLIC_MONGODB_URI;
-        if(uri)
-        {
-            const client = new MongoClient(uri, {
-                serverApi: {
-                  version: ServerApiVersion.v1,
-                  strict: true,
-                  deprecationErrors: true,
-                }
-            });
-            db = client.db("Next-Auth");
-            return db;
-        }
-        console.log("URI not found!");
-        
-    }
-    catch(err)
-    {
-        console.log(err);
-    }
-}
+export const connectDB = async () => {
+  if (db) return db;
+
+  try {
+    const uri = `mongodb+srv://${process.env.NEXT_PUBLIC_MONGODB_URI_USERNAME}:${process.env.NEXT_PUBLIC_MONGODB_URI_PASSWORD}@cluster0.2vsxcvm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+    const client = new MongoClient(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      },
+    });
+    db = client.db("Next-Auth");
+    return db;
+
+  } catch (err) {
+    console.log(err);
+  }
+};
